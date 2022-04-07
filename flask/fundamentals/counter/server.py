3f73b20c0f10ac571064072f33194b8d5521fff1
antran1245 <antran1245@gmail.com>
2022-04-07 08:47:36 -0700

    sensei bonus on Counter

--- flask/fundamentals/counter/server.py
+++ flask/fundamentals/counter/server.py
@@ -6,20 +6,31 @@ app.secret_key = 'secret'
 @app.route('/', methods=['POST'])
 def visit():
     counter = 0
+    visited = 0
     if not session.get("count") is None:
         counter = session["count"]
+    if not session.get("visited") is None:
+        visited = session["visited"]
     if request.method == "POST":
         print(request.method)
-        if request.form.get("add") == "Add + 2":
+        if request.form.get("add") == "Add":
+            num = request.form.get("num")
+            if(num != ""):
+                counter += (int(num)-1)
+        elif request.form.get("add") == "Add + 2":
             counter += 1
         elif request.form.get("reset") == "Reset":
             counter = 0
         session["count"] = counter
+        visited -= 1
+        session["visited"] = visited
         return redirect(url_for('visit'))
     else:
         counter += 1
+        visited += 1
     session["count"] = counter
-    return render_template('index.html', counter=counter)
+    session["visited"] = visited
+    return render_template('index.html', counter=counter, visited=visited)
 
 
 @app.route('/destroy_session')
