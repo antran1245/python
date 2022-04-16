17f911ff1d14944ef86f0095cad88e8662da865e
antran1245 <antran1245@gmail.com>
2022-04-16 13:16:05 -0700

    initial setup of private wall

---
+++ flask_mysql/validation/private_wall/flask_app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+app = Flask(__name__)
+app.secret_key = "loginREgristation"
+bcrypt = Bcrypt(app)
+DATABASE = 'private_wall_schema'
+from flask_app.controllers import users
